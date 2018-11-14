a40e31be062f3a86c7538d422240717f3c1cff58
lgbin879 <lgbin879@163.com>
2018-11-14 17:42:59 +0800

    add natural/chromatic parm and support

--- fretboardTraining.py
+++ fretboardTraining.py
@@ -44,12 +44,28 @@ notePath = './ogg/noteSpell/'
 soundPath = './ogg/pitchSound/piano/'
 
 stringList = []
-natrualList = []
+natrualList = [
+[0, 1, 3, 5, 7, 8, 10, 12, 13, 15, 17, 19, 20], #string-1
+[0, 1, 3, 5, 6, 8, 10, 12, 13, 15, 17, 18, 20], #string-2
+[0, 2, 4, 5, 7, 9, 10, 12, 14, 16, 17, 19],     #string-3
+[0, 2, 3, 5, 7, 9, 10, 12, 14, 15, 17, 19],     #string-4
+[0, 2, 3, 5, 7, 8, 10, 12, 14, 15, 17, 19, 20], #string-5
+[0, 1, 3, 5, 7, 8, 10, 12, 13, 15, 17, 19, 20]  #string-6
+]
+chromaticList = [
+[2, 4, 6, 9, 11, 14, 16, 18],                   #string-1
+[2, 4, 7, 9, 11, 14, 16, 19],                   #string-2
+[1, 3, 6, 8, 11, 13, 15, 18, 20],               #string-3
+[1, 4, 6, 8, 11, 13, 16, 18, 20],               #string-4
+[1, 4, 6, 9, 11, 13, 16, 18],                   #string-5
+[2, 4, 6, 9, 11, 14, 16, 18]                    #string-6
+]
 
 
 def text2voice(noteName, file):
     text = noteName.replace('#', ' sharp')
-    url = 'http://tts.baidu.com/text2audio?idx=1&tex=input&cuid=baidu_speech_demo&cod=2&lan=en&ctp=1&pdt=1&spd=4&per=2&vol=5&pit=9'.replace('input', text)
+    url = 'http://tts.baidu.com/text2audio?idx=1&tex=input&cuid=baidu_speech_demo'\
+            '&cod=2&lan=en&ctp=1&pdt=1&spd=1&per=2&vol=5&pit=9'.replace('input', text)
     
     print('wget "%s" -O %s.mp3'%(url, noteName), file=file)
 
@@ -87,7 +103,7 @@ def getNoteName(string, fret):
 
 
 def fretBoardInit():
-    #file = open('get_voice.sh', 'w+')
+    file = open('get_voice.sh', 'w+')
 
     for s in range(1, 7):
         fretList = []
@@ -99,16 +115,16 @@ def fretBoardInit():
             fretsDict['noteName'] = getNoteName(s, f)
             fretsDict['soundSrc'] = str(fretsDict['noteName'])+'.ogg'
 
-            #text2voice(fretsDict['noteName'], file)
+            text2voice(fretsDict['noteName'], file)
 
             fretList.append(fretsDict)
 
-        print('\n######String-%d######'%(s))
-        print(fretList)
+        #print('\n######String-%d######'%(s))
+        #print(fretList)
 
         stringList.append(fretList)
 
-    #file.close()
+    file.close()
     #print(stringList)
 
 
@@ -118,7 +134,8 @@ def pygameInit():
 
 
 def main(args):
-    print('\n######### Guitar String-%s, frets-%s, delay %d secs ########\n'%(args.strings, args.frets, args.delay))
+    print('\n######### Guitar String-%s, frets-%s, delay %d secs ########\n'
+        %(args.strings, args.frets, args.delay))
 
     strStart = 1
     strEnd = 6
@@ -133,25 +150,75 @@ def main(args):
             print('## Error ## : frets input out of range %s'%(args.frets))
             exit()
 
-    numStart = 0
-    numEnd = 11
+    # pitch repeat after 12 frets, so 11 frets by default
+    fretStart = 0
+    fretEnd = 11
 
     if args.frets:
         rangeStr = re.findall(r"\d+\.?\d*",args.frets)
-        numStart = int(rangeStr[0])
-        numEnd = int(rangeStr[-1])
+        fretStart = int(rangeStr[0])
+        fretEnd = int(rangeStr[-1])
         print('## Info ## : frets range %s'%(args.frets))
 
-        if numStart < 0 or numStart > 20 or numEnd < 0 or numEnd > 20 or numStart > numEnd:
+        if fretStart < 0 or fretStart > 20 or fretEnd < 0 or fretEnd > 20 or fretStart > fretEnd:
             print('## Error ## : frets input out of range %s'%(args.frets))
             exit()
 
+    if args.natrual:
+        print('\n#### Natrual Pitches Only ####\n')
+        print(natrualPitches)
+    elif args.chromatic:
+        print('\n#### Chromatic Pitches Only ####\n')
+        print(list(set(twelvePitches)-set(natrualPitches)))
+    else:
+        print('\n#### All 12 Pitches ####\n')
+        print(twelvePitches)
 
     while(True):
-      #char = random.choice(pitchRange)
+        #char = random.choice(pitchRange)
 
-      s = random.randint(strStart, strEnd)
-      f = random.randint(numStart, numEnd)
+        s = random.randint(strStart, strEnd)
+
+        if args.natrual or args.chromatic:
+            pitchList = natrualList if args.natrual else chromaticList
+            print(pitchList)
+
+            if fretStart in pitchList[s-1]:
+                idxStart = pitchList[s-1].index(fretStart)
+            elif (fretStart+1) in pitchList[s-1]:
+                idxStart = pitchList[s-1].index(fretStart+1)
+            else:
+                idxStart = pitchList[s-1].index(fretStart+2)
+
+            if fretEnd in pitchList[s-1]:
+                idxEnd = pitchList[s-1].index(fretEnd)
+            elif (fretEnd-1) in pitchList[s-1]:
+                idxEnd = pitchList[s-1].index(fretEnd-1)
+            else:
+                idxEnd = pitchList[s-1].index(fretEnd-2)
+
+            f = random.choice(pitchList[s-1][idxStart:(idxEnd+1)])
+        else:
+            f = random.randint(fretStart, fretEnd)
+
+
+        fullPath = soundPath+stringList[s-1][f]['soundSrc'] 
+        p = subprocess.Popen(["mplayer", fullPath], stdout=subprocess.PIPE)
+
+        time.sleep(2)
+
+        print('\n## Info ## : ', stringList[s-1][f], '----------------------', 
+            stringList[s-1][f]['noteName'], '\n')
+
+        noteName = stringList[s-1][f]['noteName']
+        noteOgg = noteName+'.ogg'
+        fullPath = notePath+noteOgg
+        p = subprocess.Popen(["mplayer", fullPath], stdout=subprocess.PIPE)
+
+        time.sleep(args.delay)
+
+'''
+      ## pygame play has sound transformation, so use mplayer instead
 
       sound = pygame.mixer.Sound(soundPath+stringList[s-1][f]['soundSrc'])
       sound.play()
@@ -163,27 +230,22 @@ def main(args):
       time.sleep(2)
 
       noteSound.play()
-
-      print('## Info ## : ', stringList[s-1][f], '----------------------', 
-            stringList[s-1][f]['noteName'], '\n')
-
-      time.sleep(args.delay)
-
+'''
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='random generate A-G pitches and play the sound')
     parser.add_argument("-i", "--instrument", help="instrument: piano/guitar")
     parser.add_argument("-s", "--strings", help="strings number: 1-6")
     parser.add_argument("-f", "--frets", help="specify frets range like 0-12")
-    parser.add_argument("-d", "--delay", default=3, type=int, nargs='?', help="delay secs after bee")
-    parser.add_argument("-w", "--word", help="character sound A-E", action="store_true")
+    parser.add_argument("-d", "--delay", default=5, type=int, nargs='?', help="delay secs after bee")
+    parser.add_argument("-w", "--word", help="character sound A-G", action="store_true")
     parser.add_argument("-m", "--music", help="play pitch sound", action="store_true")
     parser.add_argument("-n", "--natrual", help="natrual pitches", action="store_true")
-    parser.add_argument("-a", "--allpitch", help="all twelve pitches", action="store_false")
+    parser.add_argument("-c", "--chromatic", help="chromatic pitches", action="store_true")
 
     args = parser.parse_args()
 
-    pygameInit()
+    #pygameInit()
     fretBoardInit()
 
     main(args)
